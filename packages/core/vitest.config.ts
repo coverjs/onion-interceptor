2a1920b8e3240a1ef210b428a97a2e933b540046
EricWXY <ericwxy@foxmail.com>
2024-08-06 11:14:30 +0800

    chore: add test job to deploy workflow

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,20 +1,21 @@
 /// <reference types="vitest" />
 
-import path from 'path'
-import { defineConfig } from 'vite'
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
     coverage: {
-      include: ['src']
-    }
-  }
-})
+      include: ["src"],
+      reporter: ["lcov", "text"],
+    },
+  },
+});
